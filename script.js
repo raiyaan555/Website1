b1a04c6cbdea8e9e5f84d23fbf7b480c2d726a9c
Raiyaan Khan <kraiyaan@gmail.com>
2020-12-27 12:23:53 +0530

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+ burger = document.querySelector('.burger')
+ navbar = document.queryselctor('.navbar')
+ burger =document.querySelector('.burger')
+
+ burger.addEventListener('click',()=>{
+     navbar.classList.toggle('v-class-resp');
+     navList.clasList.toggle('v-class-resp');
+     navbar.classList.toggle('h-nav-resp');
+     
+
+ })
